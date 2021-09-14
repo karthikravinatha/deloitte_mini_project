4d3f5be32c69f31939c06ccd599e81acbb90e3d9
karthikravinatha <47776651+karthikravinatha@users.noreply.github.com>
2021-09-14 08:32:54 +0530

    Create urls.py
    added urls info.

---
+++ urls.py
@@ -0,0 +1,16 @@
+Metrics
+add: localhost:8000/add
+update: localhost:8000/update
+delete: localhost:8000/delete
+get: localhost:8000/get+blogs
+get by user: localhost:8000/get_by_user
+get by tag: localhost:8000/
+
+User
+add: localhost:8000/user_add
+update: localhost:8000/user_update
+delete: localhost:8000/user_delete
+
+Tags
+add: localhost:8000/add_tags
+delete: localhost:8000/delete_tags
